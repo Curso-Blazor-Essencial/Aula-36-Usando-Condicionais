3be9c98a4f43bf2b264fa528ba0604b81fbbddee
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-11 13:41:08 -0300

    Aula 36
    Completa

---
+++ BlazorApp_Lacos/BlazorApp_Lacos/Shared/Entidades/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp_Lacos.Shared.Entidades
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
